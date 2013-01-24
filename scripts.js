c563e92811ebf780836f88d0bb70aa35634fcd22
JonAlf <jdyrlandweaver@Aristotle.local>
2013-01-24 13:32:40 -0500

    text scales to fit

--- scripts.js
+++ scripts.js
@@ -5,24 +5,53 @@ function loadWall() {
     var cols = 15;
     var bricknum = 0;
     var html = "";
+    var name = "";
 
+    //add the hidden div for resizing
+    $("<div />", {id: "resizer"}).hide().appendTo(document.body);
+
+    //get the regular size of the brick
+    var x = $("<div class=\"brick\" />").hide().appendTo(document.body);
+    var goalWidth = $(".brick").width();
+    x.remove();
+
+    
     for ( var r=0; r < rows; r++ ) {
 	html+= "<tr>";
 	for ( var c=0; c < cols; c++ ) {
-	    html+= "<td><div class=\"brick hoverable\" id=\"" + bricknum + "\"> ";
-	    if ( bricknum == 0 ) 
-		html+= "Deirdre<br>Weaver</div></td>";
-	    else if ( bricknum == 1 )
-		html+= "Sarah<br>Hemmert</div></td>";
-	    else if ( bricknum == 76 )
-		html+= "JonAlf<br>Dyrland-Weaver</div></td>";
-	    else
-		html+= "SMC<br>Alumnus</div></td>";
+	    html+= "<td><div class=\"brick hoverable\" id=\"" + bricknum + "\"></div></td> ";
 	    bricknum++;
 	}
+	html+= "</tr>";
     }    
-    $("#wall_table").html( html );
+    $("#wall_table").html( html );    
+
+    var numBricks = bricknum;
+    for (bricknum = 0; bricknum < numBricks; bricknum++ ) {
+	if ( bricknum == 0 ) 
+	    name = "Deirdre<br>Weaver";
+	else if ( bricknum == 1 )
+	    name = "Sarah<br>Hemmert";
+	else if ( bricknum == 76 )
+	    name = "JonAlf<br>Dyrland-Weaver";
+	else
+	    name = "SMC<br>Alumnus";
+	resizeBrick( goalWidth, name, bricknum, 14 );		
+    }
+}
+
+function resizeBrick( goalWidth, name, bricknum, size ) {
+
+    var resizer = $("#resizer");
+    resizer.css("font-size", size);
+    resizer.html( name );    
+
+    while( resizer.width() > goalWidth ) {
+	console.log( size );
+	resizer.css("font-size", size--);
+    }
     
+    $("#" + bricknum).css("font-size", size).html(resizer.html());
 }
 
 function enlarge(event) {
@@ -30,9 +59,10 @@ function enlarge(event) {
     var holder  = $("#holder");
     var pos = $(this).offset();
 
-    console.log( this );
+    //resizing occurs here
+    resizeBrick( elm.width(), $(this).html(), "enlarger", 26 );		
+    //    elm.html( $(this).html() );
 
-    elm.html( $(this).html() );
     elm.css( "top", pos.top );
     elm.css( "left", pos.left );
     elm.css("visibility","visible");
